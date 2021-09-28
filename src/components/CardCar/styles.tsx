a89ce6a9737fa9f78371d40057a22ce27ff45e6a
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-09-28 16:08:09 -0300

    Incremented, fastImage

--- src/components/CardCar/styles.tsx
+++ src/components/CardCar/styles.tsx
@@ -2,6 +2,8 @@ import { RectButton } from 'react-native-gesture-handler'
 import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
+import FastImage from 'react-native-fast-image'
+
 export const Container = styled(RectButton)`
   width: 100%;
   height: 126px;
@@ -63,7 +65,7 @@ export const Type = styled.View`
 
 `
 
-export const CarImage = styled.Image`
+export const CarImage = styled(FastImage)`
   width: 167px;
   height: 85px;
 `
